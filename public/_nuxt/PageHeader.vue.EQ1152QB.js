859f9e3ab672dc112ab5e168d51e27e922600136
travelspun <scott@travelspun.com>
2024-01-18 11:52:48 -0800

    Add files via upload

---
+++ public/_nuxt/PageHeader.vue.EQ1152QB.js
@@ -0,0 +1,1 @@
+import{f as o,r as c,o as r,g as s,w as l,t,a as n}from"./entry.qYQKh60x.js";const _=o({__name:"PageHeader",props:{content:{type:Object,required:!0}},setup(e){return(i,p)=>{const a=c("v-container");return r(),s(a,{class:"mt-10 mb-5",align:"center"},{default:l(()=>[n("h2",null,t(e.content.title),1),n("p",null,t(e.content.text),1)]),_:1})}}});export{_};
